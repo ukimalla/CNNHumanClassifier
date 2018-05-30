600d9de957d0ef7d3bfc514aea4a70a757786888
admin <kangsg10@naver.com>
2018-05-31 02:54:06 +0900

    data.zip(+data.json), main.py has been updated

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import scipy.io as sio
+import codecs, json
 
-matlabfile = sio.loadmat('imdb.mat')
+f = codecs.open('data.json', 'r', 'utf-8-sig')
+data = json.load(f)
+f.close()
+
+
+for d in data["data"]:
+    print("index : " + str(d["index"]))
+    print("path : " + str(d["path"]))
+    print("name : " + str(d["name"]))
+    print("gender : " + str(d["gender"]))
+    print("age : " + str(d["age"]))
+    print("face_location : " + str(d["face_location"]))
+    print("\n")
