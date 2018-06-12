e614b08fb9b97ab9bab8119b0de470c209d91d17
ukimalla <uki.malla@gmail.com>
2018-06-12 22:51:29 +0900

    Minor Changes

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -2,14 +2,17 @@ from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
 def to_categorical(y_labels, bins):
-    if y_labels.shape[1] == 2:
-        y_age = y_labels[:, 0]
-        y_gender = y_labels[:, 1]
 
+    y_age = y_labels[:, 0]
+    y_gender = y_labels[:, 1]
 
     y_labels = np.digitize(y_age, bins=bins, right=False).reshape(-1,1)
 
     ohe = OneHotEncoder()
     y_labels = ohe.fit_transform(y_labels).toarray()
 
-    return  y_labels, y_gender
+    # for i in range(len(y_labels)):
+    #     y_labels[i].
+
+
+    return y_labels, y_gender
