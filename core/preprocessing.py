002840d4cf6fcc81c3cf66d5fe9d6b7479680d8e
Uki Malla <uki.malla@gmail.com>
2018-06-13 00:35:13 +0900

    Minor changes

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -4,15 +4,20 @@ import numpy as np
 def to_categorical(y_labels, bins):
 
     y_age = y_labels[:, 0]
-    y_gender = y_labels[:, 1]
+    y_gender = y_labels[:, 1].astype('float32')
 
     y_labels = np.digitize(y_age, bins=bins, right=False).reshape(-1,1)
 
     ohe = OneHotEncoder()
-    y_labels = ohe.fit_transform(y_labels).toarray()
+    y_age = ohe.fit_transform(y_labels).toarray()
 
-    # for i in range(len(y_labels)):
-    #     y_labels[i].
+    y_labels = []
 
+    for i in range(len(y_age)):
+        y_labels.append([y_age[0], y_gender[0]])
 
-    return y_labels, y_gender
+    return np.array(y_labels)
+
+def y_age_gender_split(y_train, y_test):
+    # Returnns train_age, train_gender, test_age, test_gender
+    return y_train[:, 0], y_train[:, 1], y_test[:, 0], y_test[:, 1]
